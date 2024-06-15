993ed9c5225ec2bf2356e251561cc85ff91ecab2
John Norén <super.happy.email.fun@gmail.com>
2024-06-15 22:24:49 +0200

    test

--- colors/greyzen.lua
+++ colors/greyzen.lua
@@ -24,14 +24,14 @@ require('lualine').setup { options = { theme = 'greyzen' } }
 vim.api.nvim_create_user_command('ToggleLineNrColor', function()
         local theme_colors = require('lush_theme.greyzen')
         local current_ln_fg = vim.api.nvim_get_hl_by_name('LineNr', true).fg
-        if current_ln_fg == theme_colors.LineNr.fg then
-            vim.api.nvim_set_hl(0, 'LineNr', { fg = theme_colors.Normal.bg })
-            vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = theme_colors.Normal.bg })
-            vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = theme_colors.Normal.bg })
+        if current_ln_fg == tostring(theme_colors.LineNr.fg) then
+            vim.api.nvim_set_hl(0, 'LineNr', { fg = tostring(theme_colors.Normal.bg) })
+            vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = tostring(theme_colors.Normal.bg) })
+            vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = tostring(theme_colors.Normal.bg) })
         else
-            vim.api.nvim_set_hl(0, 'LineNr', { fg = theme_colors.LineNr.fg })
-            vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = theme_colors.LineNrAbove.fg })
-            vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = theme_colors.LineNrBelow.fg })
+            vim.api.nvim_set_hl(0, 'LineNr', { fg = tostring(theme_colors.LineNr.fg) })
+            vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = tostring(theme_colors.LineNrAbove.fg) })
+            vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = tostring(theme_colors.LineNrBelow.fg) })
         end
     end,
     { nargs = 0, desc = 'Toggle line number color' }
